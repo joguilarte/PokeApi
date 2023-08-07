53e19d5f1da80b2b9f8016e85c25edaa3bead87c
joguilarte <33516868+joguilarte@users.noreply.github.com>
2023-08-07 01:24:11 -0300

    Update Pokes.py

--- Pokes.py
+++ Pokes.py
@@ -7,11 +7,20 @@ from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 import random
+import configparser
+import logging
 
 app = Flask(__name__)
 
+# Configurar el registro de Flask
+logging.basicConfig(level=logging.INFO)
+
+# Leer el archivo de configuración
+config = configparser.ConfigParser()
+config.read('config.cfg')
+
 # Configuración de Flask-JWT-Extended
-app.config['JWT_SECRET_KEY'] = 'super-secret'  # Es un secreto ;) 
+app.config['JWT_SECRET_KEY'] = config['DEFAULT']['JWT_SECRET_KEY']
 jwt = JWTManager(app)
 
 # Configuración de Flask-SQLAlchemy
@@ -34,7 +43,7 @@ class User(db.Model):
 
 with app.app_context():
     db.create_all()
-
+  
 
 BASE_URL = "https://pokeapi.co/api/v2/"
 
@@ -45,9 +54,11 @@ def login():
     password = request.json.get('password', None)
     user = User.query.filter_by(username=username).first()
     if not user or not user.check_password(password):
+        app.logger.warning(f'Intento fallido de inicio de sesión para el usuario {username}')
         return jsonify({"msg": "Credenciales incorrectas"}), 401
 
     access_token = create_access_token(identity=username)
+    app.logger.info(f'Usuario {username} inició sesión con éxito')
     return jsonify(access_token=access_token)
 
 # Endpoint para registrar un nuevo usuario
@@ -57,32 +68,40 @@ def register():
     current_user = get_jwt_identity()
     user = User.query.filter_by(username=current_user).first()
     if not user.is_admin:
+        app.logger.warning(f'Usuario {current_user} intentó registrar un nuevo usuario sin permisos de administrador')
         return jsonify({"msg": "No tienes permisos para realizar esta acción"}), 403
 
     username = request.json.get('username', None)
     password = request.json.get('password', None)
     if not username or not password:
+        app.logger.warning(f'Faltan datos para registrar un nuevo usuario')
         return jsonify({"msg": "Faltan datos"}), 400
 
     user = User(username, password)
     db.session.add(user)
     db.session.commit()
+    
+    app.logger.info(f'Usuario {username} registrado con éxito por el administrador {current_user}')
+    
     return jsonify({"msg": "Usuario registrado con éxito"})
 
 
-#  Listar usuarios 
+# Endpoint para listar usuarios 
 @app.route('/list_users', methods=['GET'])
 @jwt_required()
 def list_users():
     current_user = get_jwt_identity()
     user = User.query.filter_by(username=current_user).first()
     if not user.is_admin:
+        app.logger.warning(f'Usuario {current_user} intentó listar usuarios sin permisos de administrador')
         return jsonify({"msg": "No tienes permisos para realizar esta acción"}), 403
 
     users = User.query.all()
     users_list = [{"username": u.username, "is_admin": u.is_admin} for u in users]
+    
+    app.logger.info(f'Usuario {current_user} listó usuarios con éxito')
+    
     return jsonify(users_list)
-
 
 # Endpoint para eliminar un usuarios
 @app.route('/delete_user', methods=['DELETE'])
@@ -91,45 +110,77 @@ def delete_user():
     current_user = get_jwt_identity()
     user = User.query.filter_by(username=current_user).first()
     if not user.is_admin:
+        app.logger.warning(f'Usuario {current_user} intentó eliminar un usuario sin permisos de administrador')
         return jsonify({"msg": "No tienes permisos para realizar esta acción"}), 403
 
     username = request.json.get('username', None)
     if not username:
+        app.logger.warning(f'Faltan datos para eliminar un usuario')
         return jsonify({"msg": "Faltan datos"}), 400
 
     user = User.query.filter_by(username=username).first()
     if not user:
+        app.logger.warning(f'El usuario {username} no existe')
         return jsonify({"msg": "El usuario no existe"}), 404
 
     db.session.delete(user)
     db.session.commit()
+    
+    app.logger.info(f'Usuario {username} eliminado con éxito por el administrador {current_user}')
+    
     return jsonify({"msg": "Usuario eliminado con éxito"})
 
+# Endpoint para obtener los tipos de un Pokémon segun su nombre
 @app.route("/pokemon/type/<name>")
 @jwt_required()
 def get_pokemon_type(name):
+    # Verificar que el parámetro name solo contenga letras
+    if not name.isalpha():
+        return jsonify({"msg": "El nombre del Pokémon solo debe contener letras"}), 400
+    
     response = requests.get(f"{BASE_URL}pokemon/{name}")
     data = response.json()
     types = [t["type"]["name"] for t in data["types"]]
+    
+    app.logger.info(f'Tipos de Pokémon {name} obtenidos con éxito')
+    
     return jsonify(types)
 
+# Endpoint para obtener un Pokémon aleatorio de un tipo específico
 @app.route("/pokemon/random/<type>")
 @jwt_required()
 def get_random_pokemon(type):
+    # Verificar que el parámetro type solo contenga letras
+    if not type.isalpha():
+        return jsonify({"msg": "El tipo de Pokémon solo debe contener letras"}), 400
+    
     response = requests.get(f"{BASE_URL}type/{type}")
     data = response.json()
     pokemon = data["pokemon"]
     random_pokemon = random.choice(pokemon)["pokemon"]["name"]
+    
+    app.logger.info(f'Pokémon aleatorio de tipo {type} obtenido con éxito: {random_pokemon}')
+    
     return jsonify(random_pokemon)
 
+# Endpoint para obtener el Pokémon con el nombre más largo de un tipo específico
 @app.route("/pokemon/longest-name/<type>")
 @jwt_required()
 def get_longest_name_pokemon(type):
+    # Verificar que el parámetro type solo contenga letras
+    if not type.isalpha():
+        return jsonify({"msg": "El tipo de Pokémon solo debe contener letras"}), 400
+    
     response = requests.get(f"{BASE_URL}type/{type}")
     data = response.json()
     pokemon = data["pokemon"]
     longest_name_pokemon = max(pokemon, key=lambda p: len(p["pokemon"]["name"].split("-")[0]))["pokemon"]["name"].split("-")[0]
+    
+    app.logger.info(f'Pokémon con el nombre más largo de tipo {type} obtenido con éxito: {longest_name_pokemon}')
+    
     return jsonify(longest_name_pokemon)
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
